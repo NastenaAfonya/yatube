55a0eae06937b6cffdce6dbd93928cfe1ceb0780
AfanasevaAV <afanasevaav01@gmail.com>
2023-12-23 01:31:41 +0300

    добавлены директории для хранения статистических файлов

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# задаём произвольный URL, который будет использоваться для запросов к статическим файлам
+STATIC_URL = "/static/" 
+# теперь логотип можно будет запросить по адресу sitename.ex**/static/**images/logo.png
+
+# задаём адрес директории, куда командой *collectstatic* будет собрана вся статика
+STATIC_ROOT = [
+    BASE_DIR / "static",
+    ],
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
