33e6389daa92fea0098e9885919ebb99f3aca8da
AfanasevaAV <afanasevaav01@gmail.com>
2023-12-31 20:26:49 +0300

    Добавлена модель данных Group

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,3 +5,10 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE, related_name="posts" )
+    group = models.ForeignKey(Group)
+    
+class Group(models.Model):
+    title = models.TextField() # Имя группы
+    slug = models.TextField() # уникальный адрес группы, часть URL (например, для группы любителей котиков slug будет равен cats: group/cats)
+    description = models.TextField() # текст, который появляется на странице сообщества
+    
